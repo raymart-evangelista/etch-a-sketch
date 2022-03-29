11f58ac1b9e79fba1b32e7e13960eaa6d58c15fb
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-03-28 17:08:38 -0700

    add range slider in index.html to  change grid size; add functions for creating webpage grid to javascript.js

--- javascript.js
+++ javascript.js
@@ -6,18 +6,91 @@ const optContainer = document.querySelector('#optContainer');
 const gridContainer = document.querySelector('#gridContainer');
 
 
-
+///////////////////////////////
+// Creating Buttons         ///
+///////////////////////////////
 const eraseGridButton = document.createElement('button');
 eraseGridButton.textContent = 'Erase All';
 optContainer.appendChild(eraseGridButton);
 
+
+///////////////////////////////
+// Creating Grid Size Slider //
+///////////////////////////////
+const gridSizeSlider = document.getElementById('gridRange');
+const sliderValue = document.getElementById('sliderValue');
+
+sliderValue.textContent = gridSizeSlider.value;
+
+// When value on slider changes, update web page text and grid
+gridSizeSlider.oninput = function() {
+    sliderValue.textContent = this.value;
+    //numToSquare = this.value;
+    if(numToSquare !== this.value) {
+        numToSquare = this.value;
+        console.log(`new numToSquare: ${numToSquare}`);
+
+        // update gridSize
+        removeGrid();
+        createSquareDivs(numToSquare);
+        createGrid(numToSquare);
+
+    }
+}
+
+function removeGrid() {
+
+    // remove old elements and recreate grid
+    squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.remove()
+    })
+
+}
+
+function createSquareDivs(numToSquare) {
+
+    amtSquares = numToSquare ** 2;
+    // use a for loop to create grid squares and put them onto the web page
+    for (i = 0; i < amtSquares; i++) {
+        let div = document.createElement('div');
+        div.className = 'square';
+        gridContainer.appendChild(div);
+    }
+    
+}
+
+function createGrid(numToSquare) {
+
+    // flexBasis calculation
+    gridCalculation = 100 / numToSquare;
+
+
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', function (e) {
+            e.target.style.background = 'blue';
+    
+            // erase color after certain amount of time
+            setTimeout(function() {
+                // e.target.style.background = '';
+              }, 500);
+    
+        });
+    
+        square.style.flexBasis = `${gridCalculation}%`;
+    
+    });
+}
+
+
 // determine number of squares which will also determine the number of rows
-const numToSquare = 6;
-const amtSquares = numToSquare ** 2;
+let numToSquare = 16;
+let amtSquares = numToSquare ** 2;
 
 // use a for loop to create grid squares and put them onto the web page
 for (i = 0; i < amtSquares; i++) {
-    const div = document.createElement('div');
+    let div = document.createElement('div');
     div.className = 'square';
     gridContainer.appendChild(div);
 }
